f6897db53ec4515289250cbf740581b8acbe36a5
Elias Lazcano Castro Neto <elias304162@gmail.com>
2021-06-06 14:47:15 -0400

    Criando um Model (e de bônus a Migration dele)
    O comando "php artisan make:model SiteContato -m" cria um Model e também gera um arquivo Migration. O arquivo Migration serve para modificar o banco de dados, facilitando que o banco de Produção receba essas mudanças sem muito trabalho, sem executar Queries.

---
+++ database/migrations/2021_06_06_012857_create_site_contatos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiteContatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_contatos', function (Blueprint $table) {
+            $table->id(); //Cria uma coluna INT auto-incremento como Chave Primária
+            $table->timestamps(); //Cria duas colunas, datetime de criação do registro, datetime da modificação do registro.
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_contatos');
+    }
+}
